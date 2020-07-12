275d1318f6706e186f85c272a1196ae49a98e87f
Mohit Gupta <mohitguptamt123@gmail.com>
2020-07-12 13:40:00 +0530

    changes in Gear class 	added data new variables 		rim, tire 	added new behaviour 		gear_inches() 	modified constructor

--- example1/Bicycle.java
+++ example1/Bicycle.java
@@ -1,23 +1,29 @@
 package bicycle;
 
 class Gear {
-    private int chainring, cog;
+    private float chainring, cog, rim, tire;
 
-    Gear(int chainring, int cog) {
+    Gear(float chainring, float cog, float rim, float tire) {
         this.chainring = chainring;
         this.cog = cog;
+        this.rim = rim;
+        this.tire = tire;
     }
 
     public float ratio() {
-        return chainring / (float)cog;
+        return chainring / cog;
+    }
+
+    public float gear_inches() {
+        return ratio() * (rim + (tire * 2));
     }
 }
 
 public class Bicycle {
 
     public static void playWithGear() {
-        Gear gear = new Gear(52, 11);
-        System.out.println(gear.ratio());
+        Gear gear = new Gear(52.0F, 11.0F, 26.0F, 1.5F);
+        System.out.println(gear.gear_inches());
     }
     public static void main(String[] args) {
         System.out.println("\nFunction: playWithGear()");
