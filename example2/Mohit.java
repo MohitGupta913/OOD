07d8a576eab58143a56884a26f33dc3208aade2b
Mohit Gupta <mohitguptamt123@gmail.com>
2020-07-15 20:18:01 +0530

    changes in Mohit.java file: 	added data variable in bicycle class 	changed constructor signature 	defined a function to give value to tire_size according to the style of bicycle

--- example2/Mohit.java
+++ example2/Mohit.java
@@ -5,12 +5,27 @@ class Bicycle {
     private String tape_color;
     private String chain;
     private String tire_size;
+    private String style;
+    private String front_shock;
+    private String rear_shock;
 
-    public Bicycle(String size, String tape_color) {
+    public Bicycle(String size, String tape_color, String style, String front_shock, String rear_shock) {
         this.size = size;
         this.tape_color = tape_color;
-        chain = "10-speed";
-        tire_size = "23";
+        this.style = style;
+        this.front_shock = front_shock;
+        this.rear_shock = rear_shock;
+    }
+
+    public void spares() {
+        if(style.equals("road")) {
+            chain = "10-speed";
+            tire_size = "23";
+        }
+        else {
+            chain = "10-speed";
+            tire_size = "2.1";
+        }
     }
 
     public String getSize() {
@@ -22,7 +37,8 @@ class Bicycle {
 public class Mohit {
 
     public static void playWithBike() {
-        Bicycle bike =  new Bicycle("M", "red");
+        Bicycle bike =  new Bicycle("M", "red", "mountain", "Manitou", "Fox");
+        bike.spares();
         System.out.println(bike.getSize());
     }
     public static void main(String[] args) {
